16bf4c8afbd114705555b787f66ad35bb426b1e3
Kobert Lam <kobertlam@gmail.com>
2022-01-11 13:03:43 -0500

    setup GitHub Pages

--- plots.js
+++ plots.js
@@ -1,75 +1,59 @@
-// Plot simple line chart
-Plotly.newPlot("plotArea_Line", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+function init() {
+    // Select the dropdown menu
+    var selector = d3.select("#selDataset");
+  
+    // Read data from JSON file
+    d3.json("samples.json").then((data) => {
+      console.log(data);
 
-// Plot simple bar chart
-var trace = {
-    x: ["burrito", "pizza", "chicken"],
-    y: [10, 18, 5],
-    type: "bar"
+      // Assign the 'names' array to a new variable
+      var sampleNames = data.names;
+
+      // Loop through each element in the 'sampleNames' array,
+      // For each element in the array, a dropdown menu option is appended:
+      // -> 'text' of each dropdown menu option is the ID
+      // -> Its value property is also assigned the ID.
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+    });
 };
 
-var layout = {
-    title: "Luncheon Survey",
-    xaxis: {title: "Food Option"},
-    yaxis: {title: "Number of Respondents"}
+// This function takes an ID number as input argument
+function buildMetadata(sample) {
+  // Pull the entire dataset in JSON file
+  d3.json("samples.json").then((data) => {
+    // Get the metadata array from the dataset
+    var metadata = data.metadata;
+    // Filter for an object in the array with 'id'property matched the input argument
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    // Since the results of filter() method are returned as an array,
+    // the first item in the array is selected and assigned with object variable
+    var result = resultArray[0];
+    // Select 'Demographic Info' panel with id = 'sample-metadata'
+    var PANEL = d3.select("#sample-metadata");
+    // Clear the contents of thew panel
+    PANEL.html("");
+
+    // Get the key-value pair of demographic info from the select object
+    var demographicInfo = Object.entries(result);
+    
+    // For each key-value pair:
+    demographicInfo.forEach(([key, value]) =>
+      // Append a H6 heading with 'location' to the panel
+      PANEL.append("h6").text(key.toUpperCase() + ": " + value)
+    );
+  });
 };
 
-// Plot another bar chart
-Plotly.newPlot("plotArea_Bar", [trace], layout);
+function optionChanged(newSample) {
+  // The demographic information panel is populated with a specific volunteer's information
+  buildMetadata(newSample);
+  // The volunteer's data is visualized in a separate 'div'
+  //buildCharts(newSample);
+};
 
-var trace_2 = {
-    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: "bar"
-   };
-var data_2 = [trace_2];
-
-var layout_2 = {
-    title: "'Bar' Chart",
-    xaxis: {title: "Drinks"},
-    yaxis: {title: "% of Drinks Ordered"}
-   };
-Plotly.newPlot("plotArea_Bar2", data_2, layout_2);
-
-// Plot a pie chart
-var trace_3 = {
-    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
-    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: 'pie'
-  };
-  var data_3 = [trace_3];
-  var layout_3 = {
-    title: "'Pie' Chart",
-  };
-  Plotly.newPlot("plotArea_Pie", data_3, layout_3);
-
-  // Plot a scatter plot
-  var trace_4a = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
-    mode: 'markers',
-    type: 'scatter'
-  };
-  
-  var trace_4b = {
-    x: [2, 3, 4, 5],
-    y: [16, 5, 11, 9],
-    mode: 'lines',
-    type: 'scatter'
-  };
-  
-  var trace_4c = {
-    x: [1, 2, 3, 4],
-    y: [12, 9, 15, 12],
-    mode: 'lines+markers',
-    type: 'scatter'
-  };
-  
-  var data_4 = [trace_4a, trace_4b, trace_4c];
-  
-  var layout_4 = {
-    title: "Scatter Plot"
-   };
-
-  Plotly.newPlot('plotArea_Scatter', data_4, layout_4);
+init();
